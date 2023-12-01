ca4beaea85b403773b8f1a2738eeb5b8bcb34033
paulseamer <paulseamer@nhs.net>
2023-12-01 22:26:38 +0000

    Initial commit of app files

---
+++ app/js/index.js
@@ -0,0 +1,75 @@
+/* import variables */
+import { plotTitleSpacer, margin, width, height, transDur, blobDir, filePrefix, fileExt } from './main.js'
+/* import d3 functions */
+import { d3 } from './main.js'
+import { xScale, colorPal, tooltip } from './main.js'
+/* import udf functions */
+import { mouseOver, mouseOut } from './main.js'
+import { plotHsaGrps } from './main.js'
+import { updatePlots } from './main.js'
+import { switchPod } from './main.js'
+import { switchArea } from './main.js'
+import { toggleHsa } from './main.js'
+
+const selectedArea = 'E08000026'
+const selectedProjVar = '1'
+const selectedHorizon = '2025'
+const selectedPod = 'aae'
+
+const data = await d3.csv(blobDir + filePrefix + selectedArea + fileExt)
+
+const grpDat = d3.group(
+  data,
+  (d) => d.proj_id,
+  (d) => d.end_year,
+  (d) => d.pod
+)
+
+const projDat = grpDat
+  .get(selectedProjVar)
+  .get(selectedHorizon)
+  .get(selectedPod)
+
+plotHsaGrps(projDat)
+
+/* hack */
+d3.selectAll('.circle').style('opacity', 0)
+
+/* when the area dropdown changes, run switchArea() with the new value */
+d3.select('#selectArea').on('change', function () {
+  let selectedArea = d3.select(this).property('value')
+  let selectedProjVar = d3.select('#selectProjVar').property('value')
+  let selectedHorizon = d3.select('#selectHorizon').property('value')
+  let selectedPod = d3.select('#selectPod').property('value')
+  switchArea(selectedArea, selectedProjVar, selectedHorizon, selectedPod)
+})
+
+/* when the pod dropdown changes, run switchPod() with the new value */
+d3.select('#selectPod').on('change', function () {
+  let selectedPod = d3.select(this).property('value')
+  let selectedProjVar = d3.select('#selectProjVar').property('value')
+  let selectedHorizon = d3.select('#selectHorizon').property('value')
+  switchPod(data, selectedProjVar, selectedHorizon, selectedPod)
+})
+
+/* when the model horizon dropdown changes, run updatePlots() with the new value */
+d3.select('#selectHorizon').on('change', function () {
+  let selectedHorizon = d3.select(this).property('value')
+  let selectedProjVar = d3.select('#selectProjVar').property('value')
+  let selectedPod = d3.select('#selectPod').property('value')
+  updatePlots(grpDat, selectedProjVar, selectedHorizon, selectedPod)
+})
+
+/* when the projection variant dropdown changes, run updatePlots() with the new value */
+d3.select('#selectProjVar').on('change', function () {
+  let selectedProjVar = d3.select(this).property('value')
+  let selectedHorizon = d3.select('#selectHorizon').property('value')
+  let selectedPod = d3.select('#selectPod').property('value')
+  updatePlots(grpDat, selectedProjVar, selectedHorizon, selectedPod)
+})
+
+/* when the health status toggle changes show/hide circles */
+d3.select('#toggleHsa').on('change', function () {
+  const opacity = this.checked ? 1 : 0
+  toggleHsa(opacity)
+})
